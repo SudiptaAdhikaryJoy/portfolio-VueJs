3136e924a81d67420b0c96b201b6bfb93869c857
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-08-10 20:50:55 +0600

    add some comonents

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,20 +5,24 @@
                 <v-subheader class="text-h4 justify-center">Skills</v-subheader>
                 <p></p>
             </v-col>
-            <v-col lg="6">
+            <v-col lg="8">
                 <div class="text-center d-flex">
+                    <v-progress-circular :value="80" :size="120" :width="15" color="#26C6DA"></v-progress-circular>
+                    <p class="py-16">HTML5</p>
+                    <v-progress-circular :value="80" :size="120" :width="15" color="#AFB42B"></v-progress-circular>
+                    <p class="py-16">CSS3</p>
                     <v-progress-circular :value="80" :size="120" :width="15" color="blue-grey"></v-progress-circular>
                     <p class="py-16">MySQL</p>
                     <v-progress-circular :value="80" :size="120" :width="15" color="deep-orange lighten-2">
                     </v-progress-circular>
-                    <h3 class="py-5">MySQL</h3>
+                    <h3 class="py-16">Javascript</h3>
                     <v-progress-circular :value="60" :size="120" :width="15" color="brown"></v-progress-circular>
-                    <p class="py-5">MySQL</p>
-                    <v-progress-circular :value="40" :size="120" :width="15" color="lime"></v-progress-circular>
-                    <p class="py-5">MySQL</p>
-                    <v-progress-circular :value="20" :size="120" :width="15" color="indigo darken-2">
+                    <p class="py-16">ReactJs</p>
+                    <v-progress-circular :value="80" :size="120" :width="15" color="teal"></v-progress-circular>
+                    <p class="py-16">NodeJs</p>
+                    <v-progress-circular :value="70" :size="120" :width="15" color="indigo darken-2">
                     </v-progress-circular>
-                    <p class="py-5">MySQL</p>
+                    <p class="py-16">MongoDB</p>
                 </div>
             </v-col>
         </v-row>
